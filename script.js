1b32c8abad6a3927b6731c2841075422620fd6d8
AT <abel-tsegay@hotmail.com>
2024-09-20 22:16:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+var studentName = "Abel";
+var studentAge = 31;
+
+var yes = true;
+var no = false;
+var isEnrolled = [yes];
+
+var message =  `hello my name is ${studentName} and i am ${studentAge} years old and I attend Humber ${isEnrolled} ` ;
+console.log(message);
+
+var name = "John";
+console.log(name);
+
+var phoneNumber = 5193132121;
+console.log(phoneNumber);
+
+var correctNumber=true;
+var wrongNumber= false;
+var providedNumber = [wrongNumber];
+console.log(providedNumber);
+
+var schoolName= ['Humber', 'Fanshawe','Ryerson'];
+schoolName.push('Western');
+console.log(schoolName [2]);
+
+var person = {
+  height:6,
+  weight: 210, 
+};
+console.log(person.weight);
+
+console.log(25*2);
+console.log(50-10);
+console.log(40+10);
+console.log(50/2);
+
+var x = 5; 
+var y = 10;
+console.log(x > y);
+console.log( x < y);
+console.log(x === y);
+console.log(x !== y);
+
+
+
+let a = true
+var b = false
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
